16f9688b306d55faef9c07cc917f316bbe2a9e93
Leanny01 <dleannykalangu@gmail.com>
2024-04-04 16:34:37 +0200

    Added form validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,16 +7,42 @@ import { redirect } from 'next/navigation';
 const prisma = getPrismaClient();
 
 const FormSchema = z.object({
-  id: z.string(),
+  id: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
   customerId: z.string().uuid(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'],{
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -42,7 +68,20 @@ export async function createInvoice(formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(id: string,prevState: State, formData: FormData) {
+
+  const validateFields = UpdateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  if(!validateFields.success){
+    return{
+      errors: validateFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.'
+    }
+  }
   const { customerId, amount, status } = UpdateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -50,22 +89,22 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
 
   const amountInCents = amount * 100;
-try{
-  await prisma.invoices.update({
-    where: {
-      id,
-    },
-    data: {
-      customer_id: customerId,
-      amount: amountInCents,
-      status,
-    },
-  });
-}catch(error){
-    return{
-        message: `Error updating invoice`,
-    }
-}
+  try {
+    await prisma.invoices.update({
+      where: {
+        id,
+      },
+      data: {
+        customer_id: customerId,
+        amount: amountInCents,
+        status,
+      },
+    });
+  } catch (error) {
+    return {
+      message: `Error updating invoice`,
+    };
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
